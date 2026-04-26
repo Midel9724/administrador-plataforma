be4d0f9247761eff8dc266ce41bca85a41f9b84c
Midel9724 <arelo1357924@email.com>
2026-04-25 22:14:16 -0600

    scripts de autenticación y actualiza configuración del servidor

--- server.js
+++ server.js
@@ -114,5 +114,7 @@ app.get('/api/courses/:id/reviews', (req, res) => {
 });
 
 
-
-app.listen(3000, () => console.log('Servidor corriendo en http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
